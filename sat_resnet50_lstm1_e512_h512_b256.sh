54e7404a686ef10b453a3010526b315f272a36d0
shahdev <devanshshah7560@gmail.com>
2019-12-12 17:28:54 -0600

    running CUDA script -- thanks dev

--- sat_resnet50_lstm1_e512_h512_b256.sh
+++ sat_resnet50_lstm1_e512_h512_b256.sh
@@ -4,9 +4,9 @@
 #SBATCH --output="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
 #SBATCH --error="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
 #SBATCH --partition=gpux1
-#SBATCH --cpus-per-gpu=16
-#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:v100:1
+##SBATCH --cpus-per-gpu=16
+#SBATCH --time=24:00:00
 
 source /opt/apps/anaconda3/etc/profile.d/conda.sh 
 conda activate image_captions
