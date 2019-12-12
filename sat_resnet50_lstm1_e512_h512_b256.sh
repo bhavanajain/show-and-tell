42d0b13b895630622fe9e7e8db6516613c3d81ac
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-12-12 16:17:48 -0600

    minor fix

--- sat_resnet50_lstm1_e512_h512_b256.sh
+++ sat_resnet50_lstm1_e512_h512_b256.sh
@@ -13,7 +13,7 @@ set job_details="sat_resnet50_lstm1_e512_h512_b256"
 source /opt/apps/anaconda3/etc/profile.d/conda.sh 
 conda activate image_captions
 
-python train.py 
+python train.py \
 	--models_dir %job_details% \
 	--vocab_path vocab2014.pkl \
 	--image_root ../CS-547/image_captions/train2014 \
