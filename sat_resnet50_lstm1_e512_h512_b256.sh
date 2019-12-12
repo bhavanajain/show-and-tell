05a17b9897215539083d825b4bef9489e2ce7196
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-12-12 16:34:38 -0600

    fix

--- sat_resnet50_lstm1_e512_h512_b256.sh
+++ sat_resnet50_lstm1_e512_h512_b256.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=sat_resnet50_lstm1_e512_h512_b256
+#SBATCH --job-name="sat_resnet50_lstm1_e512_h512_b256"
 #SBATCH --output="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
 #SBATCH --error="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
 
 #SBATCH --partition=gpux1
 #SBATCH --cpus-per-gpu=16
-#SBATCH --time=36:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:v100:1
 
 source /opt/apps/anaconda3/etc/profile.d/conda.sh 
@@ -17,3 +17,5 @@ srun python train.py \
 	--vocab_path vocab2014.pkl \
 	--image_root ../CS-547/image_captions/train2014 \
 	--captions_json ../CS-547/image_captions/annotations/captions_train2014.json \
+
+
