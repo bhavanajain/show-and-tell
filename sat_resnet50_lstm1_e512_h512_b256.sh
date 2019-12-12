cdf289ad1984f9742a8db4538762a8e6334b43e8
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-12-12 17:19:14 -0600

    dev script

--- sat_resnet50_lstm1_e512_h512_b256.sh
+++ sat_resnet50_lstm1_e512_h512_b256.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name="sat_resnet50_lstm1_e512_h512_b256"
 #SBATCH --output="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
 #SBATCH --error="sat_resnet50_lstm1_e512_h512_b256.%j.%N.out"
-
 #SBATCH --partition=gpux1
 #SBATCH --cpus-per-gpu=16
 #SBATCH --time=24:00:00
