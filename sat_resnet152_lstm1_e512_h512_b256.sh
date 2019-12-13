800cf01d23276ccca91cf5269c8806f946290040
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-12-12 19:06:15 -0600

    resnet152 script

---
+++ sat_resnet152_lstm1_e512_h512_b256.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name="sat_resnet152_lstm1_e512_h512_b256"
+#SBATCH --output="sat_resnet152_lstm1_e512_h512_b256.%j.%N.out"
+#SBATCH --error="sat_resnet152_lstm1_e512_h512_b256.%j.%N.out"
+#SBATCH --partition=gpux1
+#SBATCH --gres=gpu:v100:1
+##SBATCH --cpus-per-gpu=16
+#SBATCH --time=36:00:00
+
+source /opt/apps/anaconda3/etc/profile.d/conda.sh 
+conda activate image_captions
+
+srun python train.py \
+	--models_dir sat_resnet152_lstm1_e512_h512_b256 \
+	--vocab_path vocab2014.pkl \
+	--image_root ../CS-547/image_captions/train2014 \
+	--captions_json ../CS-547/image_captions/annotations/captions_train2014.json \
+	--resnet_size 152 \
+	--rnn_type lstm
+
+
