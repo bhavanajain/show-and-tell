06c32e999c0050b8cfbed764abda21ec02c3b26a
Bhavana Jain <cs14btech11008@iith.ac.in>
2019-12-14 19:52:42 -0600

    add beam search code

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from torch.nn.utils.rnn import pack_padded_sequence
+from utils import torch_tile
 
 class Encoder(nn.Module):
 	def __init__(self, resnet_size, image_shape, embed_size):
@@ -70,22 +71,6 @@ class Decoder(nn.Module):
 
 		return outputs
 
-	def sample_single(self, image_embedding, caption_maxlen):
-		caption_word_ids = []
-		input_embedding = image_embedding.unsqueeze(1)
-		for i in range(caption_maxlen):
-			if i == 0:
-				hidden, state = self.rnn(input_embedding)
-			else:
-				hidden, state = self.rnn(input_embedding, state)
-			output = self.linear(hidden.squeeze(1))
-			max_val, predicted_index = output.max(1)
-			caption_word_ids.append(predicted_index.item())
-
-			input_embedding = self.embedding(predicted_index).unsqueeze(1)
-
-		return caption_word_ids
-
 	def sample_batch(self, image_embeddings, caption_maxlen):
 		caption_word_ids = []
 		input_embeddings = image_embeddings.unsqueeze(1)
@@ -104,6 +89,61 @@ class Decoder(nn.Module):
 
 		caption_word_ids = torch.stack(caption_word_ids, 1)
 		return caption_word_ids
+
+	def sample_beam(self, features, states=None, beam_size=1, device=None, vocab=None):
+		"""Generate captions for given image features using beam search."""
+		sampled_ids = []
+		features = features.unsqueeze(1)
+		inputs = features.repeat(beam_size,1,1)        
+		k_sampled_ids = [[] for i in range(beam_size)] #k full sentences
+		
+		probs = torch.ones(beam_size, 1).to(device) #batch_size*1
+		batch_size = beam_size #to begin with, then as a sentence ends, batch_size will decrease by 1
+		finished_k_sample_ids = []
+		for i in range(self.max_seg_length):
+			hiddens, states = self.rnn(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+			outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+			outputs = torch.nn.functional.softmax(outputs, dim=1)
+			sentence_probs = torch.mul(outputs, probs) #(batch_size, vocab_size)
+			if i==1:
+				res, ind = sentence_probs[0].view(-1).topk(beam_size)   
+			else:
+				res, ind = sentence_probs.view(-1).topk(beam_size)
+
+			new_indices = ind/outputs.shape[1]
+			new_words = ind%outputs.shape[1] #batch_size*1
+			extract_indices = []
+			new_k_sampled_ids = []
+			for j in range(batch_size):
+				extended_sentence = k_sampled_ids[new_indices[j].item()] + [new_words[j]]
+				if vocab.idx2word[new_words[j].item()]=='<end>':
+					finished_k_sample_ids.append((extended_sentence, sentence_probs[new_indices[j]][new_words[j]]))
+					finished_k_sample_ids.sort(key=lambda x:-x[1])
+					batch_size -= 1
+					if batch_size == 0:
+						return [finished_k_sample_ids[0][0]], finished_k_sample_ids[0][1]
+				else:
+					new_k_sampled_ids.append(extended_sentence)
+					extract_indices.append(j)
+
+			k_sampled_ids = [x for x in new_k_sampled_ids]
+
+			new_indices = new_indices[extract_indices]
+			res = res[extract_indices]
+
+			if self.rnn_type=='lstm':
+				states= (states[0][0][new_indices].unsqueeze(0), states[1][0][new_indices].unsqueeze(0))
+			elif self.rnn_type=='gru':
+				states = states[0][new_indices].unsqueeze(0)
+			probs = res.unsqueeze(1)
+
+			inputs = self.embed(new_words[extract_indices]) #batch_size*1
+			inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+
+		if len(finished_k_sample_ids)==0:
+			return [k_sampled_ids[0]], _
+
+		return [finished_k_sample_ids[0][0]], finished_k_sample_ids[0][1]
 
 
 
--- utils.py
+++ utils.py
@@ -13,3 +13,11 @@ def collate_fn(data):
 		targets[i, :curr_end] = caption[:curr_end]
 
 	return images, targets, lengths
+
+def torch_tile(a, dim, n_tile):
+    init_dim = a.size(dim)
+    repeat_idx = [1] * a.dim()
+    repeat_idx[dim] = n_tile
+    a = a.repeat(*(repeat_idx))
+    order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
+    return torch.index_select(a, dim, order_index)
